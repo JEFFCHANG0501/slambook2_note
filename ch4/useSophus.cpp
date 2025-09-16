6bfda5a4acdc6b2a12c33ec2bb5c124df8139503
jeff <jaychang0501@gmail.com>
2025-09-16 13:44:50 +0800

    feat: add a ch4 useSophus example

---
+++ ch4/useSophus.cpp
@@ -0,0 +1,53 @@
+#include <Eigen/Core>
+#include <iostream>
+
+#include "sophus/se3.hpp"
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+  Matrix3d R = AngleAxisd(M_PI / 2, Vector3d(0, 0, 1)).toRotationMatrix();
+
+  Quaterniond q(R);
+  Sophus::SO3d SO3_R(R);
+  Sophus::SO3d SO3_q(q);
+
+  cout << "SO(3) from matrix: \n" << SO3_R.matrix() << endl;
+  cout << "SO(3) from quaternion: \n" << SO3_q.matrix() << endl;
+  cout << "they are equal" << endl << endl;
+
+  // Use log() to get the vector
+  Vector3d so3 = SO3_R.log();
+  cout << "so3 = " << so3.transpose() << endl;
+  cout << "so3 hat = \n" << Sophus::SO3d::hat(so3) << endl;
+  cout << "so3 hat vee = "
+       << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << endl;
+
+  Vector3d update_so3(1e-4, 0, 0);
+  Sophus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
+  cout << "SO3 updated = \n" << SO3_updated.matrix() << endl;
+
+  Vector3d t(1, 0, 0);
+  Sophus::SE3d SE3_Rt(R, t);
+  Sophus::SE3d SE3_qt(q, t);
+
+  cout << "SE3 from R, t = \n" << SE3_Rt.matrix() << endl;
+  cout << "SE3 from q, t = \n" << SE3_qt.matrix() << endl;
+
+  Matrix<double, 6, 1> se3 = SE3_Rt.log();
+  cout << "se3 = " << se3.transpose() << endl;
+  cout << "se3 hat = \n" << Sophus::SE3d::hat(se3) << endl;
+  cout << "se3 hat vee = "
+       << Sophus::SE3d::vee(Sophus::SE3d::hat(se3)).transpose() << endl
+       << endl;
+
+  Matrix<double, 6, 1> update_se3;
+  update_se3.setZero();
+  update_se3(0, 0) = 1e-4;
+  cout << "update_se3 = " << update_se3.transpose() << endl;
+  Sophus::SE3d SE3_updated = Sophus::SE3d::exp(update_se3) * SE3_Rt;
+  cout << "SE3 updated = \n" << SE3_updated.matrix() << endl;
+
+  return 0;
+}
